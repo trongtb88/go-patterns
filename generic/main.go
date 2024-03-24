8414e8d4a04e3ad239a98758b14a603bba5f653d
trong-fs <trong.tran@fundingsocieties.com>
2024-03-24 17:23:45 +0700

    generic pattern

---
+++ generic/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/exp/constraints"
+)
+
+type Num interface {
+	int | int8 | float64
+}
+
+func Add[T Num](a, b T) T {
+	return a + b
+}
+
+type UserID int
+
+func AddUserID[T ~int](a, b T) T {
+	return a + b
+}
+
+//----------------------------------------------
+// input : [1,2,3], (n) => n * 2
+// output : [2,4,6]
+
+func mapValues[T constraints.Ordered](values []T, mapFunc func(T) T) []T {
+	var newValues []T
+	for _, v := range values {
+		newValue := mapFunc(v)
+		newValues = append(newValues, newValue)
+	}
+	return newValues
+}
+
+func main() {
+	result := Add(1.5, 3.5)
+	fmt.Printf("result %+v\n", result)
+
+	userID1 := UserID(1)
+	userID2 := UserID(2)
+	resultUserID := AddUserID(userID1, userID2)
+	fmt.Printf("resultUserID %+v\n", resultUserID)
+
+	//----------------------------------------
+	resultMap := mapValues([]int{1, 2, 3}, func(i int) int {
+		return i * 2
+	})
+
+	fmt.Printf("resultMap %+v\n", resultMap)
+
+	resultMapFloat := mapValues([]string{"a", "b", "c"}, func(i string) string {
+		return i + i
+	})
+	fmt.Printf("resultMap %+v\n", resultMapFloat)
+
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-patterns
 
 go 1.21.6
 
-require golang.org/x/sync v0.6.0 // indirect
+require (
+	golang.org/x/exp v0.0.0-20240318143956-a85f2c67cd81 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+)
